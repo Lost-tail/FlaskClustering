d8c2377aac5729a7c98abe5d5ffa85bfd963eb06
Ivan Bikeev <dead43rus@gmail.com>
2020-11-30 22:44:47 +0300

    work is neary done'

---
+++ Methods/analyze.py
@@ -0,0 +1,103 @@
+import pandas as pd
+from sklearn import metrics
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+class Analyzation():
+    def __init__(self,direct='data/'):
+        self.X = []
+        self.index=dict()
+        self.pure_data = []
+        scaler = MinMaxScaler()
+        self.km = None
+        self.dbscn=None
+        self.data = [pd.read_csv(direct+'20{}.csv'.format(i), sep=';',encoding='ANSI',lineterminator='\n').dropna(thresh=1).fillna(0) for i in range(10,19)]
+        for i in range(len(self.data)):
+            def is_float(x):
+                try:
+                    float(x)
+                    return True
+                except:
+                    return False
+            self.pure_data.append(self.data[i].drop('Регион',1).where(self.data[i].applymap(lambda x: is_float(x)),0))
+            self.X.append(scaler.fit_transform(self.data[i].drop('Регион',1).where(self.data[i].applymap(lambda x: is_float(x)),0)))
+        self.aver_param = self.pure_data[-1].applymap(lambda x: float(x))
+        for i in self.pure_data[:-1]:
+            self.aver_param+=i.applymap(lambda x: float(x))
+        self.aver_param=self.aver_param/9
+        self.aver_param['Регион']=self.data[-1]['Регион']
+    def k_means(self):
+        km = KMeans(n_clusters = 6,random_state  = 1)
+        self.km =km.fit(self.X[-1])
+        #for i in range(len(self.X)-1):
+            #self.km = self.km.fit(self.X[i])
+        for i in range(len(self.data)):
+            self.data[i]['k-means'] = self.km.predict(self.X[i])
+        self.index['k-means'] = [metrics.silhouette_score(self.X[i], self.km.labels_) for i in range(len(self.X))]
+
+    def dbscan(self):
+        """model = NearestNeighbors(n_neighbors=5)
+        for i in range(len(self.X)):
+            model.fit(self.X[i])
+        for i in range(len(self.X)):
+            dist, _ = model.kneighbors(self.X[i],n_neighbors=5,return_distance=True)
+            dist = dist[:,-1]
+            dist = np.sort(dist)
+            plt.plot(dist)
+            plt.savefig('test{}.png'.format(i))"""
+        eps = 0.7
+        self.dbscn = DBSCAN(eps=eps, min_samples = 5)
+        for i in range(len(self.X)):
+            self.data[i]['dbscan']=self.dbscn.fit_predict(self.X[i])
+        self.index['dbscan'] = [metrics.silhouette_score(self.X[i], self.dbscn.labels_) for i in range(len(self.X))]
+    def create_pictures(self):
+        self.aver_pict()
+        self.aver_pict2()
+    def aver_pict(self):
+        i=0
+        for x in self.aver_param.columns[:-1]:
+
+            data = self.aver_param.sort_values(by=x,ascending=False)
+            fig, ax = plt.subplots(figsize=(14,7))
+            plt.vlines(x=data['Регион'], ymin=0, ymax=data[x], color='firebrick', alpha=0.7, linewidth=7)
+            plt.title(x)
+            #for j,param in enumerate(data[x]):
+                #plt.text(j,param+0.5,round(param),horizontalalignment='center')
+            plt.xticks(rotation=60, horizontalalignment='right',fontsize=8)
+            ax.set_box_aspect(0.3)
+            ax.set_anchor('N')
+            fig.savefig('static/figures/'+'aver{}.png'.format(i))
+            plt.close()
+            i+=1
+    def aver_pict2(self):
+        i=0
+        for x in self.aver_param.columns[:-2]:
+            data2 = self.pure_data[-1]
+            data2['Регион']=self.data[-1]['Регион']
+            data2 = data2.sort_values(by=x,ascending=False)
+            fig,ax= plt.subplots(figsize=(14,7))
+            plt.vlines(x=data2['Регион'], ymin=0, ymax=data2[x], color='firebrick', alpha=0.7, linewidth=7)
+            plt.title(x)
+            plt.xticks(rotation=60, horizontalalignment='right',fontsize=8)
+            ax.set_box_aspect(0.3)
+            ax.set_anchor('N')
+            fig.savefig('static/figures/'+'aver_2{}.png'.format(i))
+            plt.close()
+            i+=1
+"""
+#print(Test.data[0],Test.index['k-means'])
+data = pd.read_csv('data/2018.csv', sep=';',encoding='ANSI',lineterminator='\n').dropna(thresh=3).fillna(0)
+scaler = MinMaxScaler()
+X = scaler.fit_transform(data.drop('Регион',1))
+km = KMeans(n_clusters = 6,random_state  = 1)
+algo = km.fit(X)
+data['cluster'] = algo.labels_
+#print(data)
+index = metrics.silhouette_score(X, algo.labels_)
+#plt.xticks(rotation=90)
+#plt.scatter(data['Регион'],X.mean(axis=1), c = algo.labels_)
+#plt.savefig('test.png')
+"""
--- app.py
+++ app.py
@@ -1,55 +1,111 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for, flash, request
 from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
+from Methods.analyze import Analyzation
         
-basedir = 'd:/Documents/Programming/Flasklearning/Database/'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecret'
 
 bootstrap=Bootstrap(app)
-
-class ChooseForm(FlaskForm):
-    submit1 = SubmitField('Sting')
+Analyze = Analyzation()
+Analyze.k_means()
+Analyze.dbscan()
+Analyze.create_pictures()
+class K_MeansForm(FlaskForm):
+    submit1 = SubmitField('K_Means')
+class DbscanForm(FlaskForm):
     submit2 = SubmitField('DBSCAN')
-    submit3 = SubmitField('K_Means')
+class StingForm(FlaskForm):
+    submit3 = SubmitField('Sting')
 class Choose_MethodForm(FlaskForm):
     submit = SubmitField('Выбрать данный метод основным')
 
 @app.route('/',methods=['GET', 'POST'])
 def start_page():
-    button = ChooseForm()
-    if button.validate_on_submit():
-        if button.submit1.data:
-            return redirect(url_for('sting_method'))
-        if button.submit2.data:
-            return redirect(url_for('dbscan_method'))
-        if button.submit3.data:
-            return redirect(url_for('k_means_method'))
-    return render_template('start_page.html',form=button)
-
-@app.route('/sting',methods=['GET', 'POST'])   
-def sting_method():
-    button = Choose_MethodForm()
-    if button.validate_on_submit():
-        return redirect(url_for('sting_method'))
-    return render_template('base_overall.html', method='Sting',form = button)
-
-@app.route('/dbscan',methods=['GET', 'POST'])     
-def dbscan_method():
-    button = Choose_MethodForm()
-    if button.validate_on_submit():
+    button1 = K_MeansForm()
+    button2 = DbscanForm()
+    button3 = StingForm()
+    if button1.submit1.data:
+        return redirect(url_for('k_means_method'))
+    if button2.submit2.data:
         return redirect(url_for('dbscan_method'))
-    return render_template('base_overall.html', method='DBSCAN',form = button)
-
-@app.route('/k_means',methods=['GET', 'POST'])     
-def k_means_method():
-    button = Choose_MethodForm()
-    if button.validate_on_submit():
+    if button3.submit3.data:
         return redirect(url_for('sting_method'))
-    return render_template('base_overall.html', method='K_Means',form = button)
+    return render_template('start_page.html',form1=button1,form2=button2,form3=button3)
+
+@app.route('/sting',methods=['GET'])   
+def sting_method():
+    return render_template('base_overall.html', method='Sting')
+
+@app.route('/dbscan',methods=['GET'])     
+def dbscan_method():
+    headers = Analyze.data[-1].columns[1:16]
+    index = Analyze.index['dbscan'][-1]
+    years = [ i for i in range(2010,2019)]
+    subj_numb =[[x-1]+[0]*(len(years)) for x in range(len(Analyze.data[-1].groupby('dbscan')))]
+    for j in range(len(years)):
+        i=0
+        for x in Analyze.data[j].groupby('dbscan'):
+            subj_numb[i][j+1] = len(x[1])
+            i+=1
+    return render_template('dbscan.html', method='DBSCAN',subj_numb=subj_numb,index=index,years=years)
+
+@app.route('/k_means',methods=['GET'])     
+def k_means_method():
+    headers = Analyze.data[-1].columns[1:16]
+    #clusters_centers=[]
+    clusters_center1=[]
+    i=0
+    for y in Analyze.km.cluster_centers_:
+        clusters_center1.append([i]+[interpretation(x) for x in y])
+        #clusters_centers.append([i]+[x for x in y])
+        i+=1
+    cl1 = clusters_center1
+    #clusters = sorted(clusters_centers, key = lambda x: sum(x[1:])/(len(x)-1),reverse = True)
+    #for i in range(len(clusters)):
+        #for j in range(1,len(clusters[i])):
+            #clusters[i][j] = interpretation(clusters[i][j])
+    index = Analyze.index['k-means'][-1]
+    years = [ i for i in range(2010,2019)]
+    subj_numb =[[x]+[0]*(len(years)) for x in range(6)]
+
+    for j in range(len(years)):
+        i=0
+        for x in Analyze.data[j].groupby('k-means'):
+            subj_numb[i][j+1] = len(x[1])
+            i+=1
+    return render_template('base_overall.html', method='K_Means',headers=headers,clusters=cl1,index=index,years=years,subj_numb=subj_numb)
+
+@app.route('/compare',methods=['GET'])  
+def compare():
+    index1 = Analyze.index['k-means'][-1]
+    index2 = Analyze.index['dbscan'][-1]
+    number1 = len(Analyze.data[-1].groupby('k-means'))
+    number2 = len(Analyze.data[-1].groupby('dbscan'))
+    return render_template('compare.html',index1=index1,index2=index2,number1=number1,number2=number2)
+
+@app.route('/analyze',methods=['GET']) 
+def analyze():
+    regions = Analyze.data[-1]['Регион']
+    return render_template('analyze.html',regions=regions)
+
+@app.route('/average_params',methods=['GET','POST']) 
+def average_params():
+    params = Analyze.aver_param.columns[:-1]
+    imgs1 = ["figures/aver{}.png".format(i) for i in range(15)]
+    imgs2 = ["figures/aver_2{}.png".format(i) for i in range(15)]
+    return render_template('average_params.html',params = params,imgs1=imgs1,imgs2=imgs2)
+    
+@app.route('/subject_params',methods=['GET','POST']) 
+def subject_params():
+    return render_template('subject_params.html')
+    
+@app.route('/cluster_params',methods=['GET','POST']) 
+def cluster_params():
+    return render_template('cluster_params.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -59,6 +115,17 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-      
+def interpretation(x):
+    if x < 0.2:
+        return 'низкий'
+    elif x < 0.4:
+        return 'ниже среднего'
+    elif x < 0.6:
+        return 'средний'
+    elif x < 0.8:
+        return 'выше среднего'
+    else:
+        return 'высокий'
+        
 if __name__=='__main__':
-	app.run()
+	app.run(debug=True)
