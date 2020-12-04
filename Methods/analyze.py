b5311160915165f09a5bec9ad761bd59c8ed48cc
Ivan Bikeev <dead43rus@gmail.com>
2020-12-04 18:57:58 +0300

    Final version

--- Methods/analyze.py
+++ Methods/analyze.py
@@ -1,12 +1,11 @@
 import pandas as pd
 from sklearn import metrics
-from sklearn.cluster import KMeans, DBSCAN
+from sklearn.cluster import KMeans, DBSCAN, Birch
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 import numpy as np
-from multiprocessing import Process
 import matplotlib
 matplotlib.use('Agg')
 
@@ -15,9 +14,12 @@ class Analyzation():
         self.X = []
         self.index=dict()
         self.pure_data = []
-        scaler = MinMaxScaler()
-        self.km = None
+        self.scaler = MinMaxScaler()
+        self.km = []
+        self.cl1=None
+        self.cl2=None
         self.dbscn=None
+        self.br=None
         self.data = [pd.read_csv(direct+'20{}.csv'.format(i), sep=';',encoding='ANSI',lineterminator='\n').dropna(thresh=1).fillna(0) for i in range(10,19)]
         for i in range(len(self.data)):
             def is_float(x):
@@ -27,21 +29,52 @@ class Analyzation():
                 except:
                     return False
             self.pure_data.append(self.data[i].drop('Регион',1).where(self.data[i].applymap(lambda x: is_float(x)),0))
-            self.X.append(scaler.fit_transform(self.data[i].drop('Регион',1).where(self.data[i].applymap(lambda x: is_float(x)),0)))
+            self.X.append(self.scaler.fit_transform(self.data[i].drop('Регион',1).where(self.data[i].applymap(lambda x: is_float(x)),0)))
         self.aver_param = self.pure_data[-1].applymap(lambda x: float(x))
         for i in self.pure_data[:-1]:
             self.aver_param+=i.applymap(lambda x: float(x))
         self.aver_param=self.aver_param/9
         self.aver_param['Регион']=self.data[-1]['Регион']
-    def k_means(self):
-        km = KMeans(n_clusters = 6,random_state  = 1)
-        self.km =km.fit(self.X[-1])
+    def birch(self):
+        br = Birch(n_clusters = 6)
+        self.br =br.fit(self.X[-1])
         #for i in range(len(self.X)-1):
             #self.km = self.km.fit(self.X[i])
         for i in range(len(self.data)):
-            self.data[i]['k-means'] = self.km.predict(self.X[i])
-        self.index['k-means'] = [metrics.silhouette_score(self.X[i], self.km.labels_) for i in range(len(self.X))]
-
+            self.data[i]['birch'] = self.br.predict(self.X[i])
+        self.index['birch'] = [metrics.silhouette_score(self.X[i], self.br.labels_) for i in range(len(self.X))]
+        #print(self.br.subcluster_labels_)
+    def k_means(self):
+        #km = KMeans(n_clusters = 6,random_state  = 1)
+        #self.km =km.fit(self.X[-1])
+        for i in range(len(self.X)):
+            kmeans = KMeans(n_clusters = 6,random_state  = 1)
+            self.km.append(kmeans.fit(self.X[i]))
+        for i in range(len(self.data)):
+            self.data[i]['k-means'] = self.km[i].predict(self.X[i])
+        self.index['k-means'] = [metrics.silhouette_score(self.X[i], self.km[i].labels_) for i in range(len(self.X))]
+        clusters_center1=[None]*len(self.km)
+        clusters_center2=[]
+        def interpretation(x):
+            if x < 0.2:
+                return 'низкий'
+            elif x < 0.4:
+                return 'ниже среднего'
+            elif x < 0.6:
+                return 'средний'
+            elif x < 0.8:
+                return 'выше среднего'
+            else:
+                return 'высокий'
+        for i in range(len(self.km)):
+            arr=[]
+            for y in self.km[i].cluster_centers_:
+                arr.append([interpretation(x) for x in y])
+            clusters_center1[i]=arr
+            clusters_center2.append(self.scaler.inverse_transform(self.km[i].cluster_centers_).round())
+        self.cl1 = clusters_center1
+        self.cl2 = clusters_center2
+      
     def dbscan(self):
         """model = NearestNeighbors(n_neighbors=5)
         for i in range(len(self.X)):
@@ -91,14 +124,18 @@ class Analyzation():
     def subj_param(self,active_param):
         ind = list(self.data[-1]['Регион']).index(active_param)
         i=0
-        for param in self.aver_param.columns[:-1]:
+        for param in range(len(self.aver_param.columns[:-1])):
             data = []
-            for year in self.pure_data:
-                data.append(float(year.at[ind,param]))
+            cl_data=[]
+            for year in range(len(self.pure_data)):
+                data.append(float(self.pure_data[year].at[ind,self.aver_param.columns[param]]))
+                cl_data.append(self.cl2[year][int(self.data[year].at[ind,'k-means'])][param])
             fig,ax= plt.subplots(figsize=(15,6))
             plt.plot([i for i in range(2010,2019)], data, color='firebrick')
-            plt.title(param)
+            plt.title(self.aver_param.columns[param])
+            plt.scatter([i for i in range(2010,2019)],cl_data,marker='*',s=200,label='Центр кластера субъекта')
             plt.xticks(rotation=60, horizontalalignment='right',fontsize=8)
+            fig.legend()
             fig.savefig('static/figures/subjects/{}{}.png'.format(active_param,i))
             plt.close()
             i+=1
@@ -120,19 +157,60 @@ class Analyzation():
         plt.plot([i for i in range(2010,2019)], data, color='firebrick')
         plt.xticks(rotation=60, horizontalalignment='right',fontsize=8)
         plt.title("Кластер №{}".format(clust_numb))
+        plt.ylabel('Мощность кластера')
         fig.savefig('static/figures/clusters/clust_power{}.png'.format(clust_numb))
         plt.close()
+    def clust_diff(self,clust_numb):
+        for param in range(len(self.aver_param.columns[:-1])):
+            cl_data = []
+            max_ = []
+            min_ = []
+            for y in range(9):
+                cl_data.append(self.cl2[y][clust_numb][param])
+                try:
+                    a_max=float(self.data[y].groupby('k-means')[self.aver_param.columns[param]].max()[clust_numb])
+                except:
+                    a_max = 0
+                try:
+                    a_min=float(data_cl[y].groupby('k-means')[self.aver_param.columns[param]].min()[clust_numb])
+                except:
+                    a_min=0
+                max_.append(a_max)
+                min_.append(a_min)
+            fig,ax= plt.subplots(figsize=(15,6))
+            plt.plot([i for i in range(2010,2019)], cl_data, color='firebrick')
+            plt.scatter([i for i in range(2010,2019)],max_,marker='*',s=200,label='Максимальное значение')
+            plt.scatter([i for i in range(2010,2019)],min_,marker='*',s=200,label='Минимальное значение')
+            plt.title(self.aver_param.columns[param]) 
+            fig.legend()
+            fig.savefig('static/figures/clusters/cl{}{}.png'.format(clust_numb,param))
+            plt.close()
     def vis_k_means(self):
         i=0
         for year in self.X:
             tsne = TSNE(random_state=13)
-            tsne_repr = tsne.fit_transform(year)
+            data= np.vstack((year,self.km[i].cluster_centers_))
+            tsne_repr = tsne.fit_transform(data)
             fig,ax= plt.subplots(figsize=(15,6))
-            plt.scatter(tsne_repr[:,0],tsne_repr[:,1],c=self.data[i]['k-means'].map({0:'b',1:'g',2:'y',3:'k',4:'r',5:'m'}))
+            plt.scatter(tsne_repr[:-6,0],tsne_repr[:-6,1],c=self.data[i]['k-means'].map({0:'b',1:'g',2:'y',3:'k',4:'r',5:'m',6:'c'}))
+            plt.scatter(tsne_repr[-6:,0],tsne_repr[-6:,1],c=['b','g','y','k','r','m'],marker='*',s=200)
             plt.title("Распределение кластеров в 201{} году".format(i))
             fig.savefig('static/figures/visualization/k-means201{}.png'.format(i))
             plt.close()
             i+=1
+    def vis_birch(self):
+        i=0
+        for year in self.X:
+            tsne = TSNE(random_state=13)
+            data= np.vstack((year,self.br.subcluster_centers_))
+            tsne_repr = tsne.fit_transform(data)
+            fig,ax= plt.subplots(figsize=(15,6))
+            plt.scatter(tsne_repr[:-12,0],tsne_repr[:-12,1],c=self.data[i]['birch'].map({0:'b',1:'g',2:'y',3:'k',4:'r',5:'m'}))
+            plt.scatter(tsne_repr[-12:,0],tsne_repr[-12:,1],c=pd.Series(self.br.subcluster_labels_).map({0:'b',1:'g',2:'y',3:'k',4:'r',5:'m'}),marker='*',s=200)
+            plt.title("Распределение кластеров в 201{} году".format(i))
+            fig.savefig('static/figures/visualization/birch201{}.png'.format(i))
+            plt.close()
+            i+=1      
     def vis_dbscan(self):
         i=0
         for year in self.X:
@@ -144,5 +222,6 @@ class Analyzation():
             fig.savefig('static/figures/visualization/dbscan201{}.png'.format(i))
             plt.close()
             i+=1
-            
+
+
 
--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, render_template, session, redirect, url_for, flash, request
-from flask_script import Manager, Shell
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
-from Methods.analyze import Analyzation
-from multiprocessing import Process        
+from Methods.analyze import Analyzation     
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'supersecret'
@@ -13,6 +11,7 @@ bootstrap=Bootstrap(app)
 Analyze = Analyzation()
 Analyze.k_means()
 Analyze.dbscan()
+Analyze.birch()
 Analyze.create_pictures()
 
 class K_MeansForm(FlaskForm):
@@ -20,7 +19,7 @@ class K_MeansForm(FlaskForm):
 class DbscanForm(FlaskForm):
     submit2 = SubmitField('DBSCAN')
 class StingForm(FlaskForm):
-    submit3 = SubmitField('Sting')
+    submit3 = SubmitField('Birch')
 class Choose_MethodForm(FlaskForm):
     submit = SubmitField('Выбрать данный метод основным')
 
@@ -34,18 +33,29 @@ def start_page():
     if button2.submit2.data:
         return redirect(url_for('dbscan_method'))
     if button3.submit3.data:
-        return redirect(url_for('sting_method'))
+        return redirect(url_for('birch_method'))
     return render_template('start_page.html',form1=button1,form2=button2,form3=button3)
     
 
-@app.route('/sting',methods=['GET'])   
-def sting_method():
-    return render_template('base_overall.html', method='Sting')
+@app.route('/birch',methods=['GET'])   
+def birch_method():
+    headers = Analyze.data[-1].columns[1:16]
+    Analyze.vis_birch()
+    imgs = ['figures/visualization/birch201{}.png'.format(i) for i in range(9)]
+    index = sum(Analyze.index['birch'])/len(Analyze.index['birch'])
+    years = [ i for i in range(2010,2019)]
+    subj_numb =[[x]+[0]*(len(years)) for x in range(len(Analyze.data[-1].groupby('birch')))]
+    for j in range(len(years)):
+        i=0
+        for x in Analyze.data[j].groupby('birch'):
+            subj_numb[i][j+1] = len(x[1])
+            i+=1
+    return render_template('dbscan.html', method='Birch',subj_numb=subj_numb,index=index,years=years,imgs=imgs)
 
 @app.route('/dbscan',methods=['GET'])     
 def dbscan_method():
     headers = Analyze.data[-1].columns[1:16]
-    index = Analyze.index['dbscan'][-1]
+    index = sum(Analyze.index['dbscan'])/len(Analyze.index['dbscan'])
     Analyze.vis_dbscan()
     imgs = ['figures/visualization/dbscan201{}.png'.format(i) for i in range(9)]
     years = [ i for i in range(2010,2019)]
@@ -62,36 +72,37 @@ def k_means_method():
     headers = Analyze.data[-1].columns[1:16]
     Analyze.vis_k_means()
     imgs = ['figures/visualization/k-means201{}.png'.format(i) for i in range(9)]
-    #clusters_centers=[]
-    clusters_center1=[]
-    i=0
-    for y in Analyze.km.cluster_centers_:
-        clusters_center1.append([i]+[interpretation(x) for x in y])
-        #clusters_centers.append([i]+[x for x in y])
-        i+=1
-    cl1 = clusters_center1
-    #clusters = sorted(clusters_centers, key = lambda x: sum(x[1:])/(len(x)-1),reverse = True)
-    #for i in range(len(clusters)):
-        #for j in range(1,len(clusters[i])):
-            #clusters[i][j] = interpretation(clusters[i][j])
-    index = Analyze.index['k-means'][-1]
+    cl1 = Analyze.cl1
+    cl2 = Analyze.cl2
+    index = sum(Analyze.index['k-means'])/len(Analyze.index['k-means'])
     years = [ i for i in range(2010,2019)]
     subj_numb =[[x]+[0]*(len(years)) for x in range(6)]
-
+    length1 = len(cl1[0])
+    length2 = len(cl1[0][0])
+    print(length1,length2)
     for j in range(len(years)):
         i=0
         for x in Analyze.data[j].groupby('k-means'):
             subj_numb[i][j+1] = len(x[1])
             i+=1
-    return render_template('base_overall.html', method='K_Means',headers=headers,clusters=cl1,index=index,years=years,subj_numb=subj_numb,imgs=imgs)
+    return render_template('base_overall.html', method='K_Means',headers=headers,cl1=cl1,index=index,years=years,subj_numb=subj_numb,imgs=imgs,cl2=cl2,length1=length1,length2=length2)
 
 @app.route('/compare',methods=['GET'])  
 def compare():
-    index1 = Analyze.index['k-means'][-1]
-    index2 = Analyze.index['dbscan'][-1]
-    number1 = len(Analyze.data[-1].groupby('k-means'))
-    number2 = len(Analyze.data[-1].groupby('dbscan'))
-    return render_template('compare.html',index1=index1,index2=index2,number1=number1,number2=number2)
+    index1 = Analyze.index['k-means']
+    index2 = Analyze.index['dbscan']
+    index3 = Analyze.index['birch']
+    aver_ind1 =sum(Analyze.index['k-means'])/len(Analyze.index['k-means'])
+    aver_ind2 =sum(Analyze.index['dbscan'])/len(Analyze.index['dbscan'])
+    aver_ind3 =sum(Analyze.index['birch'])/len(Analyze.index['birch'])
+    years = [ i for i in range(2010,2019)]
+    if max(aver_ind1,aver_ind2,aver_ind3)==aver_ind1:
+        best = 'k-means'
+    elif max(aver_ind1,aver_ind2,aver_ind3)==aver_ind2:
+        best = 'dbscan'
+    else:
+        best = 'Birch'
+    return render_template('compare.html',index1=index1,index2=index2,index3=index3,aver_ind1=aver_ind1,aver_ind2=aver_ind2,aver_ind3=aver_ind3,best=best,years=years)
 
 @app.route('/analyze',methods=['GET']) 
 def analyze():
@@ -118,11 +129,13 @@ def subject_params():
     
 @app.route('/cluster_params',methods=['GET','POST']) 
 def cluster_params():
-    clust_numb = int(request.args.get('clust_numb'))
-    if clust_numb or clust_numb==0:
-        Analyze.clust_power(clust_numb)
-    img = 'figures/clusters/clust_power{}.png'.format(clust_numb)
-    return render_template('cluster_params.html',clusters=[i for i in range(6)],img=img)
+    clust_numb = request.args.get('clust_numb')
+    if clust_numb or clust_numb=="0":
+        Analyze.clust_power(int(clust_numb))
+        Analyze.clust_diff(int(clust_numb))
+    img1 = 'figures/clusters/clust_power{}.png'.format(clust_numb)
+    imgs2 = ['figures/clusters/cl{}{}.png'.format(clust_numb,param) for param in range(len(Analyze.aver_param.columns[:-1]))]
+    return render_template('cluster_params.html',clusters=[i for i in range(6)],img1=img1,imgs2=imgs2)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -132,17 +145,6 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-def interpretation(x):
-    if x < 0.2:
-        return 'низкий'
-    elif x < 0.4:
-        return 'ниже среднего'
-    elif x < 0.6:
-        return 'средний'
-    elif x < 0.8:
-        return 'выше среднего'
-    else:
-        return 'высокий'
         
 if __name__=='__main__':
 	app.run(debug=True)
